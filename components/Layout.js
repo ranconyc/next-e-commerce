fdf794dd929e7b7e28ff162818938b6051faa904
Ran Cohen <rancohen@rans-mbp.myfiosgateway.com>
2021-11-22 12:06:47 -0500

    update styles

--- components/Layout.js
+++ components/Layout.js
@@ -1,21 +1,23 @@
 import React, { useState } from 'react';
 import Head from 'next/head';
 import { AppBar, Toolbar, Container, Typography } from '@material-ui/core';
+import useStyles from '../utils/styles';
 
 export default function Layout({ children, ...props }) {
   const [appName, setAppName] = useState('Next e-commerce');
+  const classes = useStyles();
   return (
     <div>
       <Head>
         <title>{appName}</title>
       </Head>
-      <AppBar position="static">
+      <AppBar position="static" className={classes.navbar}>
         <Toolbar>
           <Typography>{appName}</Typography>
         </Toolbar>
       </AppBar>
-      <Container>{children}</Container>
-      <footer>
+      <Container className={classes.main}>{children}</Container>
+      <footer className={classes.footer}>
         <Typography>All rights reserved {appName}</Typography>
       </footer>
     </div>

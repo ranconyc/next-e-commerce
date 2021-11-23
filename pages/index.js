e50748253f1bdcf845e83a924c8514788c779b92
Ran Cohen <rancohen@rans-mbp.myfiosgateway.com>
2021-11-23 00:05:23 -0500

    fetch products

--- pages/index.js
+++ pages/index.js
@@ -11,15 +11,17 @@ import {
 import React from 'react';
 import NextLink from 'next/link';
 import Layout from '../components/Layout';
-import data from '../utils/data';
+import db from '../utils/db';
+import Product from '../models/product';
+import Axios from 'axios';
 
-export default function Home() {
+export default function Home({ products, ...props }) {
   return (
     <Layout>
       <div>
         <h1>Products</h1>
         <Grid container spacing={3}>
-          {data.products.map((product) => (
+          {products.map((product) => (
             <Grid item md={4} key={product.name}>
               <Card>
                 <NextLink href={`/product/${product.slug}`} passHref>
@@ -47,4 +49,12 @@ export default function Home() {
       </div>
     </Layout>
   );
+}
+
+export async function getServerSideProps() {
+  await db.connect();
+  const products = await Product.find({}).lean();
+  await db.disconnect();
+
+  return { props: { products: products.map(db.convertDocToObj) } };
 }
